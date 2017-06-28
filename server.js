b9daa5fcab4375fe6fa57a427d10326e9b74ecbe
Simon Renoult <simon.renoult@gmail.com>
2017-06-28 17:35:37 +0200

    feat: add rules

--- server.js
+++ server.js
@@ -1,4 +1,9 @@
 const express = require('express')
+const favicon = require('serve-favicon')
+const path = require('path')
 const app = express()
+
+app.use(favicon(path.join(__dirname, 'public', 'favicon.ico')))
+app.use(express.static('public'))
 
 module.exports = app
